bc924797224758f1fb9983919d6812b671724a34
Jignesh120404 <120722412+Jignesh120404@users.noreply.github.com>
2023-01-07 15:36:06 +0530

    Add files via upload

---
+++ deploy.js
@@ -0,0 +1,27 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+
+const provider = new HDWalletProvider(
+  'reward ability journey bitter cause laugh unhappy glory risk garlic black lesson',
+  // remember to change this to your own phrase!
+  'https://goerli.infura.io/v3/139b1db49de443099fa3c8b0731f8363'
+  // remember to change this to your own endpoint!
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
